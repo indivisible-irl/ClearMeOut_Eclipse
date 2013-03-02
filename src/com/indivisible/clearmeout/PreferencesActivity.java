4b95451b847448ba6cde0223cfb4f7ec2fdf1f9a
indivisible-irl <mbspare@gmail.com>
2013-03-02 11:10:27 +0000

    reworked preference dependencies and (de)activations

--- src/com/indivisible/clearmeout/PreferencesActivity.java
+++ src/com/indivisible/clearmeout/PreferencesActivity.java
@@ -2,7 +2,6 @@ package com.indivisible.clearmeout;
 
 import com.mburman.fileexplore.FileExplore;
 
-import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
@@ -12,8 +11,8 @@ import android.preference.EditTextPreference;
 import android.preference.Preference;
 import android.preference.Preference.OnPreferenceClickListener;
 import android.preference.PreferenceActivity;
+import android.preference.PreferenceManager;
 import android.util.Log;
-import android.view.inputmethod.InputMethodManager;
 import android.widget.Toast;
 
 
@@ -22,26 +21,26 @@ public class PreferencesActivity extends PreferenceActivity implements OnPrefere
 {
 
 	//// data
-	private static final String strInterval = "Clear the folder every %d minutes";   //TODO get string res
+	
 	private static final String TAG = "CMO:PreferencesActivity";
 	private final int REQUEST_CODE_PICK_DIR = 1;
 	
+	// shared preferences
+	private SharedPreferences prefs;
+	
 	// preferences
 	private CheckBoxPreference pCbServiceActive;
-	
 	private EditTextPreference pEtFolder;
 	private CheckBoxPreference pCbRecursiveDelete, pCbDeleteFolders, pCbNotifyOnDelete;
-	
 	private CheckBoxPreference pCbIntervalType;										//TODO intervalType --> list
-	private CheckBoxPreference pCbStrictInterval;
 	private EditTextPreference pEtDailyTime, pEtIntervalMinutes;
-	
+	private CheckBoxPreference pCbStrictInterval;
 	private CheckBoxPreference pCbUseExtensionFilter, pCbUsePatternFilter;
 	private CheckBoxPreference pCbDeleteExtensionMatches, pCbDeletePatternMatches;
-	private EditTextPreference pEtExtensions, pEtPattern;
+	private EditTextPreference pEtExtensionsFilter, pEtPatternFilter;
 	
-	private String pFolderKey;
-	private String pIntervalKey;
+	
+	
 	
 	//// default Activity methods
 	
@@ -49,10 +48,17 @@ public class PreferencesActivity extends PreferenceActivity implements OnPrefere
 	protected void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
-		//setContentView(R.layout.activity_settings);
-		addPreferencesFromResource(R.xml.preferences);
 		
-		initPrefs();
+		addPreferencesFromResource(R.xml.preferences);
+		prefs = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+		
+		initPreferences();
+		
+		// disable non functional preferences
+		setUnimplemenetedUnselectable();
+		
+//		// testing SharedPreference access
+//		Toast.makeText(this, "Folder is:\n" +prefs.getString(pEtFolder.getKey(), "missed"), Toast.LENGTH_SHORT).show();
 	}
 	
 	// register as a sharedPreferenceListener
@@ -60,9 +66,11 @@ public class PreferencesActivity extends PreferenceActivity implements OnPrefere
 	protected void onResume()
 	{
 		super.onResume();
+		// register pref change listener
 		getPreferenceScreen().getSharedPreferences().registerOnSharedPreferenceChangeListener(this);
 		
-		pFolder.setSummary(pFolder.getText());
+		//TODO updatePreferences() containing following and others
+		pEtFolder.setSummary(pEtFolder.getText());
 		updateIntervalSummary();
 	}
 
@@ -79,7 +87,6 @@ public class PreferencesActivity extends PreferenceActivity implements OnPrefere
 	{
 		super.onStop();
 		
-		//TODO best here or in onPause()? Called too often up there or too rarely here?
 		Intent updateIntent = new Intent(getApplicationContext(), UpdateAlarmsService.class);
 		startService(updateIntent);
 	}
@@ -87,41 +94,35 @@ public class PreferencesActivity extends PreferenceActivity implements OnPrefere
 	
 	//// custom handling of some preferences
 
-	private void initPrefs()
+	private void initPreferences()
 	{
-		pFolderKey = getString(R.string.pref_key_target_folder);
-		pIntervalKey = getString(R.string.pref_key_periodic_at);
+		pCbServiceActive = (CheckBoxPreference) findPreference(getString(R.string.pref_key_service_active));
 		
-		pFolder = (EditTextPreference) findPreference(pFolderKey);				//FIXME get @string ref
-		pInterval = (EditTextPreference) findPreference(pIntervalKey);			//FIXME get @string ref
+		pEtFolder = (EditTextPreference) findPreference(getString(R.string.pref_key_target_folder));
+		pCbRecursiveDelete = (CheckBoxPreference) findPreference(getString(R.string.pref_key_use_recursive_delete));
+		pCbDeleteFolders = (CheckBoxPreference) findPreference(getString(R.string.pref_key_delete_folders));
+		pCbNotifyOnDelete = (CheckBoxPreference) findPreference(getString(R.string.pref_key_notify_on_delete));
+		
+		pCbIntervalType = (CheckBoxPreference) findPreference(getString(R.string.pref_key_interval_type));
+		pEtDailyTime = (EditTextPreference) findPreference(getString(R.string.pref_key_daily_at));
+		pEtIntervalMinutes = (EditTextPreference) findPreference(getString(R.string.pref_key_periodic_at));
+		pCbStrictInterval =  (CheckBoxPreference) findPreference(getString(R.string.pref_key_use_strict_alarms));
+		
+		pCbUseExtensionFilter = (CheckBoxPreference) findPreference(getString(R.string.pref_key_use_filter_extensions));
+		pCbDeleteExtensionMatches = (CheckBoxPreference) findPreference(getString(R.string.pref_key_delete_files_matching_extension));
+		pEtExtensionsFilter = (EditTextPreference) findPreference(getString(R.string.pref_key_filter_extensions));
+		pCbUsePatternFilter = (CheckBoxPreference) findPreference(getString(R.string.pref_key_use_filter_pattern));
+		pCbDeletePatternMatches = (CheckBoxPreference) findPreference(getString(R.string.pref_key_delete_files_matching_pattern));
+		pEtPatternFilter = (EditTextPreference) findPreference(getString(R.string.pref_key_filter_pattern));
 		
 		// pFolder
 		Intent folderIntent = new Intent(this, FileExplore.class);
-		pFolder.setIntent(folderIntent);
-		pFolder.setOnPreferenceClickListener(this);
+		pEtFolder.setIntent(folderIntent);
+		pEtFolder.setOnPreferenceClickListener(this);
 		
 		//TODO disable filter prefs until implemented
 	}
 	
-	private void updateFolderPreference(Intent receivedIntent)
-	{
-		String selectedFolderPath = receivedIntent.getStringExtra(
-				com.vassiliev.androidfilebrowser.FileBrowserActivity.returnDirectoryParameter);
-		
-//		int lastPathSep = selectedFilePath.lastIndexOf(File.separator);
-//		String selectedFolderPath = selectedFilePath.substring(0, lastPathSep);
-		
-		pFolder.setSummary(selectedFolderPath);
-		pFolder.setText(selectedFolderPath);
-		
-//		Toast.makeText(this, "SET AS:\n\n"+pFolder.getText(), Toast.LENGTH_SHORT).show();
-	}
-
-	private void updateIntervalSummary()
-	{
-		int interval = Integer.parseInt(pInterval.getText());
-		pInterval.setSummary(String.format(strInterval, interval));
-	}
 	
 	
 	//// override onPreferenceClick and onPreferenceChange
@@ -130,29 +131,20 @@ public class PreferencesActivity extends PreferenceActivity implements OnPrefere
 	public boolean onPreferenceClick(Preference preference)
 	{
 		// Folder preference
-		if (preference.equals(pFolder))
+		if (preference.equals(pEtFolder))
 		{
-			// disable the EditText dialog (and soft keyboard) as we're using a file picking intent instead
-			//TODO prob better to disable the pref opening a dialog at all
-			InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
-			imm.toggleSoftInput(InputMethodManager.HIDE_IMPLICIT_ONLY, 0);
-//		    imm.hideSoftInputFromInputMethod(pFolder.getEditText().getApplicationWindowToken(), 0);
-			pFolder.getDialog().dismiss();
+//			// disable the EditText dialog (and soft keyboard) as we're using a file picking intent instead
+//			InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+//			imm.toggleSoftInput(InputMethodManager.HIDE_IMPLICIT_ONLY, 0);
+			pEtFolder.getDialog().dismiss();
 			
-			// create the file picking intent
-			
-			// new AndroidFileBrowser functionality
-			performFolderPreferenceClickAction();
-			
-			// removed AndroidFileExplore
-//			Intent folderIntent = new Intent(this, FileExplore.class);
-//			startActivityForResult(folderIntent, folderIntentRequestCode);
-			
+			// pick folder
+			pEtFolderGetFolder();
 			return true;
 		}
 		
 		// Interval Preference
-		else if (preference.equals(pInterval))
+		else if (preference.equals(pEtIntervalMinutes))
 		{
 			// update the preference summary with the new value
 			updateIntervalSummary();
@@ -166,70 +158,82 @@ public class PreferencesActivity extends PreferenceActivity implements OnPrefere
 	@Override
 	public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key)
 	{
-		if (key.equals(pIntervalKey))	//FIXME get @string ref
+		if (key.equals(pEtIntervalMinutes.getKey()))
 		{
 			updateIntervalSummary();
 		}
+		else if (key.equals(pCbIntervalType.getKey()))
+		{
+			updateIntervalTypesEnabled();
+		}
 		
 	}
-
 	
-	/// triggered activity result handling
+	//// custom preference updates and actions
+	//TODO see what the common actions are and method(pref) them
 	
-	@Override
-	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-		super.onActivityResult(requestCode, resultCode, data);
-		
-		if (requestCode == REQUEST_CODE_PICK_DIR) {
-        	if(resultCode == RESULT_OK) {
-        		updateFolderPreference(data);
-//        		String newDir = data.getStringExtra(
-//        				com.vassiliev.androidfilebrowser.FileBrowserActivity.returnDirectoryParameter);
-//        		Toast.makeText(
-//        				this, 
-//        				"Received DIRECTORY path from file browser:\n"+newDir, 
-//        				Toast.LENGTH_LONG).show(); 
-	        	
-        	} else {//if(resultCode == this.RESULT_OK) {
-        		Toast.makeText(
-        				this, 
-        				"Received NO result from file browser",
-        				Toast.LENGTH_LONG).show(); 
-        	}//END } else {//if(resultCode == this.RESULT_OK) {
-        }//if (requestCode == REQUEST_CODE_PICK_DIR) {
-		
-//		if (resultCode == 1)
-//		{
-//			switch (requestCode)
-//			{
-//			case folderIntentRequestCode:
-//				Log.d(TAG, "ActivityResult recieved correctly");
-//				
-//				updateFolderPreference(data);
-//				break;
-//			
-//			default:
-//				Log.e(TAG, "onActivityResult: invalid request code - " +requestCode);
-//				break;
-//			}
-//		}
-//		else
-//		{
-//			Log.w(TAG, "Received non 1 resultCode: " +resultCode);
-//		}
-	}
-
-
-
-	private void performFolderPreferenceClickAction()
+	private void updateFolderPreference(Intent receivedIntent)
 	{
-		Log.d(TAG, "Start browsing button pressed");
+		String selectedFolderPath = receivedIntent.getStringExtra(
+				com.vassiliev.androidfilebrowser.FileBrowserActivity.returnDirectoryParameter);
+		
+//		int lastPathSep = selectedFilePath.lastIndexOf(File.separator);
+//		String selectedFolderPath = selectedFilePath.substring(0, lastPathSep);
+		
+		pEtFolder.setSummary(selectedFolderPath);
+		pEtFolder.setText(selectedFolderPath);
+		
+//		Toast.makeText(this, "SET AS:\n\n"+pFolder.getText(), Toast.LENGTH_SHORT).show();
+	}
+
+	private void updateIntervalSummary()
+	{
+		int interval = Integer.parseInt(pEtIntervalMinutes.getText());
+		pEtIntervalMinutes.setSummary(String.format(getString(R.string.pref_summ_periodic_at), interval));
+	}
+	
+	private void updateIntervalTypesEnabled()
+	{
+		if (PreferenceManager.getDefaultSharedPreferences(this).getBoolean(pCbIntervalType.getKey(), true))
+		{
+			// set to daily
+			pEtDailyTime.setEnabled(true);
+			pEtIntervalMinutes.setEnabled(false);
+		}
+		else
+		{
+			// set to periodic
+			pEtDailyTime.setEnabled(false);
+			pEtIntervalMinutes.setEnabled(true);
+		}
+	}
+
+	/**
+	 * Disable interactions for currently unimplemented preferences
+	 */
+	private void setUnimplemenetedUnselectable()
+	{
+		pCbUseExtensionFilter.setSelectable(false);
+		pCbDeleteExtensionMatches.setSelectable(false);
+		pEtExtensionsFilter.setSelectable(false);
+		
+		pCbUsePatternFilter.setSelectable(false);
+		pCbDeletePatternMatches.setSelectable(false);
+		pEtPatternFilter.setSelectable(false);
+	}
+	
+	//// special case action: Get target folder - startActivityForResult() and onActivityResult()
+	
+	/**
+	 * StartActivityForResult() to get user's folder choice
+	 */
+	private void pEtFolderGetFolder()
+	{
+		Log.d(TAG, "Starting FileBrowserActivity to get target folder...");
 		Intent fileExploreIntent = new Intent(
 				com.vassiliev.androidfilebrowser.FileBrowserActivity.INTENT_ACTION_SELECT_DIR,
-				null,
-				this,
-				com.vassiliev.androidfilebrowser.FileBrowserActivity.class
-				);
+				null, this,
+				com.vassiliev.androidfilebrowser.FileBrowserActivity.class);
 		//If the parameter below is not provided the Activity will try to start from sdcard(external storage),
 		// if fails, then will start from roor "/"
 		// Do not use "/sdcard" instead as base address for sdcard use Environment.getExternalStorageDirectory() 
@@ -237,12 +241,35 @@ public class PreferencesActivity extends PreferenceActivity implements OnPrefere
 //				ua.com.vassiliev.androidfilebrowser.FileBrowserActivity.startDirectoryParameter, 
 //				"/sdcard"
 //				);
-		startActivityForResult(
-				fileExploreIntent,
-				REQUEST_CODE_PICK_DIR
-				);
+		startActivityForResult(fileExploreIntent, REQUEST_CODE_PICK_DIR);
 	}
 	
+	
+	/// triggered activity result handling
+	
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		super.onActivityResult(requestCode, resultCode, data);
+		
+		if (requestCode == REQUEST_CODE_PICK_DIR)
+		{
+        	if(resultCode == RESULT_OK)
+        	{
+        		// set pEtFolder value and summary
+        		updateFolderPreference(data);	        	
+        	}
+        	else
+        	{
+        		Log.d(TAG, "Received NO result from file browser"); 
+        	}
+        }
+		else
+		{
+			Log.w(TAG, "Received code other than REQUEST_CODE_PICK_DIR from ActivityResult");
+		}
+	}
+		
+	//TODO add a help menu option here?
 //	@Override
 //	public boolean onCreateOptionsMenu(Menu menu) {
 //		// Inflate the menu; this adds items to the action bar if it is present.
